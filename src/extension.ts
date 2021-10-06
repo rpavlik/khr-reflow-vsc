01072904fae681ecff7eb47ca5a547439f4ffaaf
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-06 17:47:54 -0500

    Use vendored files

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,31 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// import { reflowLines, stringToLines } from 'khr-reflow-ts';
+
+import { reflowLines, stringToLines } from './main';
+
+function makeFullDocument(document: vscode.TextDocument): vscode.Range {
+	return document.lineAt(document.lineCount - 1).range.with({ start: new vscode.Position(0, 0) });
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "khr-reflow-vsc" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('khr-reflow-vsc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Khronos Asciidoctor Spec Reflow!');
+	let sel: vscode.DocumentSelector = { language: 'asciidoc' };
+	vscode.languages.registerDocumentFormattingEditProvider(sel, {
+		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+			const input = stringToLines(document.getText());
+			const result = reflowLines(input, null);
+			return [vscode.TextEdit.replace(makeFullDocument(document), result)];
+		}
 	});
 
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
