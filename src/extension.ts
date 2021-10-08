6fd45d4c9284fa75d83f95d5376b12bab24b6ffe
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-08 11:43:08 -0500

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,49 @@
 
 import * as vscode from 'vscode';
 
-import { reflowLines, stringToLines } from '@ryan.pavlik/khr-reflow';
+import * as reflow from '@ryan.pavlik/khr-reflow';
 
 function makeFullDocument(document: vscode.TextDocument): vscode.Range {
 	return document.lineAt(document.lineCount - 1).range.with({ start: new vscode.Position(0, 0) });
+}
+ 
+class Reflower {
+	private state: reflow.ReflowState = new reflow.ReflowState();
+	private range: vscode.Range | null= null;
+
+	tryGetChunk(): null | { inputRange: vscode.Range, lines: string[] } {
+		if (!this.state.isBetweenParagraphs()) {
+			return null;
+		}
+		if (this.range === null) {
+			return null;
+		}
+		const lines = this.state.getEmittedLines();
+		const range = this.range;
+		this.state.clearEmittedText();
+		this.range = null;
+		return {
+			inputRange: range,
+			lines: lines,
+		};
+	}
+	processLine(document: vscode.TextDocument, lineNumber: number): boolean {
+		let line = document.lineAt(lineNumber);
+		if (this.range == null) {
+			this.range = line.rangeIncludingLineBreak;
+		} else {
+			this.range = this.range.union(line.rangeIncludingLineBreak);
+		}
+		this.state.processNumberedLine(line.text, line.lineNumber);
+		return this.state.isBetweenParagraphs();
+	}
+}
+
+
+export function formatDocument(document: vscode.TextDocument): vscode.TextEdit[] {
+	const input = reflow.stringToLines(document.getText());
+	const result = reflow.reflowLines(input, null);
+	return [vscode.TextEdit.replace(makeFullDocument(document), result)];
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -16,11 +55,18 @@ export function activate(context: vscode.ExtensionContext) {
 	let sel: vscode.DocumentSelector = { language: 'asciidoc' };
 	vscode.languages.registerDocumentFormattingEditProvider(sel, {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-			const input = stringToLines(document.getText());
-			const result = reflowLines(input, null);
-			return [vscode.TextEdit.replace(makeFullDocument(document), result)];
+			const config = vscode.workspace.getConfiguration('khr-reflow');
+			if (config.get("enable", true)) {
+				console.log("provideDocumentFormattingEdits");
+				const input = reflow.stringToLines(document.getText());
+				const result = reflow.reflowLines(input, null);
+				return [vscode.TextEdit.replace(makeFullDocument(document), result)];
+			}
+			console.log("khr-reflow disabled");
+			return [];
 		}
 	});
+	console.log("Activated khr-reflow");
 
 }
 
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -7,10 +7,13 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+ import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
+	vscode.workspace.openTextDocument({ language: "asciidoc", content: "a\nb\nc." }).then((doc) => {
+		myExtension.formatDocument(doc);
+	});
 
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
