0ab48aa5c18815948a27c51f7a44cecef0480939
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-04-27 16:48:51 -0500

    Restructure code to hopefully handle ranges.

--- src/extension.ts
+++ src/extension.ts
@@ -17,18 +17,37 @@ export function formatDocument(document: vscode.TextDocument): vscode.TextEdit[]
   return [vscode.TextEdit.replace(makeFullDocument(document), result)];
 }
 
-export function activate(context: vscode.ExtensionContext) {
-  let sel: vscode.DocumentSelector = { language: "asciidoc" };
-  vscode.languages.registerDocumentFormattingEditProvider(sel, {
-    provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+export function activate(_context: vscode.ExtensionContext) {
+  const sel: vscode.DocumentSelector = { language: "asciidoc" };
+  vscode.languages.registerDocumentRangeFormattingEditProvider(sel, {
+    provideDocumentRangeFormattingEdits(document, range, options, token): vscode.TextEdit[] {
       const config = vscode.workspace.getConfiguration("khr-reflow");
-      if (config.get("enable", true)) {
-        return formatDocument(document);
+      if (!config.get("enable", true)) {
+        // it's disabled
+        return [];
       }
-      // it's disabled
-      return [];
+      const firstLine = document.lineAt(range.start.line).range;
+      const lastLine = document.lineAt(range.end.line).range;
+
+      const expandedRange = firstLine.with({ end: lastLine.end });
+      const inputString = document.getText(expandedRange);
+      const input = reflow.stringToLines(inputString);
+      const result = reflow.reflowLines(input, null);
+      if (result === inputString) {
+        // no change
+        return [];
+      }
+      return [vscode.TextEdit.replace(expandedRange, result)];
     },
   });
+  // vscode.languages.registerDocumentFormattingEditProvider(sel, {
+  //   provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+  //     const config = vscode.workspace.getConfiguration("khr-reflow");
+  //     if (config.get("enable", true)) {
+  //       return formatDocument(document);
+  //     }
+  //     // it's disabled
+  //     return [];
+  //   },
+  // });
 }
-
-export function deactivate() {}
